a6b04fb3cc853d79fb6c0b53c02d374f70088218
Isaac T <isaactennant@Isaac-T-New.local>
2020-04-26 11:55:00 +0100

    Updates

---
+++ precache-manifest.8fa9c14f1c396f6ac6549d50bfe43fc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ce330d44460cb57b45c2c01c5c4f0aa",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "efa966c743068e639072",
+    "url": "/portfolio/static/css/main.a5477dd8.chunk.css"
+  },
+  {
+    "revision": "cd745ec2d3dc3ef7e0d6",
+    "url": "/portfolio/static/js/2.1000b7f5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.1000b7f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efa966c743068e639072",
+    "url": "/portfolio/static/js/main.dfcb3251.chunk.js"
+  },
+  {
+    "revision": "da8c0cf16d5ef46fb838",
+    "url": "/portfolio/static/js/runtime-main.fcc1d1b9.js"
+  },
+  {
+    "revision": "98e408570f61b9d85759880aa34273a8",
+    "url": "/portfolio/static/media/contact.98e40857.jpg"
+  }
+]);
