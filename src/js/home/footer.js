dcb006deb237467657d8778b50c46c9f41759506
Isaac Tennant <isaactennant@Isaacs-MBP.lan>
2020-10-26 21:31:45 +0000

    adding footer and changing homepage main structure

---
+++ src/js/home/footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import TypeIt, { TypeItInput } from "@isaac.scarrott/react-type-it";
+import "../../css/homePage.css";
+export default function Footer() {
+  return (
+    <div class="footer">
+      <h1>Footer</h1>
+    </div>
+  );
+}
--- src/js/home/homePage.js
+++ src/js/home/homePage.js
@@ -3,14 +3,11 @@ import TypeIt, { TypeItInput } from "@isaac.scarrott/react-type-it";
 import "../../css/homePage.css";
 export default function HomePage() {
   return (
-    <header className="App-header">
-      <TypeIt loop className="typeIt">
-        <TypeItInput delay={3000}>Hi, my name is Isaac Tennant</TypeItInput>
-        <TypeItInput delay={3000}>
-          I'm a software developer, well try my best to be
-        </TypeItInput>
-        <TypeItInput delay={3000}>Welcome!</TypeItInput>
-      </TypeIt>
-    </header>
+    <div className="mainPageContent">
+      <h1>Hi, my name is Isaac Tennant</h1>
+      <h2>I'm a software developer in Hull, and have been working here since graduating from the UoHull</h2>
+      <h3>I enjoy anything from bringing frontends to life, to building API's, to writing PLSQL scripts!</h3>
+      <h3>I'm currently working on this page to be my biggest project yet!</h3>
+    </div>
   );
 }
