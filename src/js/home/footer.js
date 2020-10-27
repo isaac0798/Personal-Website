2fff3b9c1e2d91a4413badfa923cc2f80ba6ba28
Isaac Tennant <isaactennant@Isaacs-MBP.lan>
2020-10-27 18:59:15 +0000

    adding footer/img to homepage

--- src/js/home/footer.js
+++ src/js/home/footer.js
@@ -4,7 +4,7 @@ import "../../css/homePage.css";
 export default function Footer() {
   return (
     <div class="footer">
-      <h1>Footer</h1>
+      <h2>isaac0798@gmail.com - <a href="https://www.linkedin.com/in/isaac-tennant-95b371164/">linkedIn</a> - <a href="https://github.com/isaac0798">Github</a></h2>
     </div>
   );
 }
--- src/js/home/homePage.js
+++ src/js/home/homePage.js
@@ -1,13 +1,16 @@
 import React from "react";
-import TypeIt, { TypeItInput } from "@isaac.scarrott/react-type-it";
 import "../../css/homePage.css";
+import homePage from "../../images/homePage.png";
+
 export default function HomePage() {
   return (
     <div className="mainPageContent">
-      <h1>Hi, my name is Isaac Tennant</h1>
-      <h2>I'm a software developer in Hull, and have been working here since graduating from the UoHull</h2>
-      <h3>I enjoy anything from bringing frontends to life, to building API's, to writing PLSQL scripts!</h3>
-      <h3>I'm currently working on this page to be my biggest project yet!</h3>
+      <div id="txtBox">
+        <h1>Hi, my name is Isaac Tennant</h1>
+        <h2>I'm a software developer in Hull, and have been working here since graduating from the UoHull.</h2>
+        <h3>I enjoy anything from bringing frontends to life, to building API's, to writing PLSQL scripts!</h3>
+      </div>
+      <img id="homePageImg" src={homePage}></img>
     </div>
   );
 }
