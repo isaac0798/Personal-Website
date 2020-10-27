d9ef7cc9493b45e109a807cc71e62e775144b0fe
Isaac Tennant <isaactennant@Isaacs-MBP.lan>
2020-10-27 19:33:15 +0000

    updating about page, adding future goal

--- src/js/about/about.js
+++ src/js/about/about.js
@@ -1,12 +1,24 @@
 import React from "react";
 import "../../css/timeline.css";
 
-const uniOfHullDescription = 'At the UoH, my dissertation was a parking finder iphone app, with a restful api in python/flask and using firebase as a database'
-const bytronDescription = 'At bytron are stack is mainly JS/PHP/Node, migrating our frontend to as much React as possible, using node microservices and moving over to more serverless using AWS'
+const uniOfHullDescription = `Going to the university of hull, was an amazing experience which really exposed me to lots of technologies. 
+  Where I mainly used C# and C++ for the university modules. I then made a mobile app for my dissertation using swift/flask/google firebase, where I learned about backend web development.`;
+const bytronDescription = `Starting as a junior, my first couple months was writing cowboy code and learning javascript 😅. Since those early days, 
+  I would like to say I have become proficient at my job as a Fullstack Developer. Since joining I've learnt JS/Node/React/TS, PHP, SQL/PLSQL which I've had to use on the job.
+  I thoroughly enjoy the work I do, and hope to continue to grow as a developer.`;
+const futureDescription = `In the future, I want to specialise in backend web development and service architecture, whilst still being able to contribute to the frontend.
+  Im currently experimenting with backend languages(currently trying ruby), Im taking a big interest in developing microservice to tackle problems and using/learning
+  NGINX to serve these up to the web under one main webapp`;
 
 export default function About() {
   return (
     <div class="timeline">
+      <TimeFrame
+        type="container right"
+        year="My Future Goals"
+        pow="??"
+        content={futureDescription}
+      />
       <TimeFrame
         type="container left"
         year="2019-Present"
