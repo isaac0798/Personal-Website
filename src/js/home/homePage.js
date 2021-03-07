2a03bd577d93be9449220b11d74e11a23be65534
Isaac Tennant <isaactennant@Isaacs-MBP.lan>
2021-03-07 10:18:48 +0000

    redisgn of portfolio

--- src/js/home/homePage.js
+++ src/js/home/homePage.js
@@ -9,13 +9,8 @@ export default function HomePage() {
       <div id="slide1" className="txtBox">
         <p>Hi, my name is Isaac Tennant</p>
         <p>I'm a software developer in Hull, and have been working here since graduating from the UoHull.</p>
-        <p>I enjoy anything from bringing frontends to life, to building API's, to writing PLSQL scripts!</p>
-        <p>Projects Below</p>
-        <img id="scrollImg" src={scroll}></img>
       </div>
-      <div id="slide2" className="txtBox">
-        <Projects />
-      </div>
+      <Projects />
     </div>
   );
 }
